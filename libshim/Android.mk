9e75e05d234672b9a41cd6966cb2e7598eee56ee
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-24 21:28:37 -0700

    kitakami: Add DisplayEventReceiver and GraphicBuffer shim for camera
    These functions changed between N and O so forward the old functions to
    their new counterparts.
    
    Change-Id: Id66dd0cc5c37e09f576b9502e7498a2f63fa04f5

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -18,10 +18,17 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := camera.qcom_shim.cpp
 
+LOCAL_SHARED_LIBRARIES := \
+    libgui \
+    liblog \
+    libui
+
+LOCAL_C_INCLUDES := \
+    frameworks/native/include
+
 LOCAL_MODULE := camera.qcom_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_32_BIT_ONLY := true
 LOCAL_PROPRIETARY_MODULE := true
 
--- libshim/camera.qcom_shim.cpp
+++ libshim/camera.qcom_shim.cpp
@@ -3,6 +3,8 @@
 #include <dlfcn.h>
 #include <string.h>
 
+#include <ui/GraphicBuffer.h>
+
 extern "C" {
   int property_get(const char * key, char * value, const char * default_value) {
     if (strcmp("ro.build.type", key) == 0) {
@@ -12,4 +14,35 @@ extern "C" {
 
     return ((int( * )(const char * , char *, const char * ))(dlsym((void * ) - 1, "property_get")))(key, value, default_value);
   }
+}
+
+extern "C" void _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
+        const native_handle_t* handle,
+        android::GraphicBuffer::HandleWrapMethod method,
+        uint32_t width,
+        uint32_t height,
+        int format,
+        uint32_t layerCount,
+        uint64_t usage,
+        uint32_t stride);
+
+extern "C" void _ZN7android13GraphicBufferC1EjjijjP13native_handleb(
+        uint32_t inWidth,
+        uint32_t inHeight,
+        int inFormat,
+        uint32_t inUsage,
+        uint32_t inStride,
+        native_handle_t* inHandle,
+        bool keepOwnership)
+{
+    android::GraphicBuffer::HandleWrapMethod inMethod =
+        (keepOwnership ? android::GraphicBuffer::TAKE_HANDLE : android::GraphicBuffer::WRAP_HANDLE);
+    _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(inHandle, inMethod, inWidth, inHeight,
+        inFormat, static_cast<uint32_t>(1), static_cast<uint64_t>(inUsage), inStride);
+}
+
+extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE();
+
+extern "C" void _ZN7android20DisplayEventReceiverC1Ev() {
+    _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE;
 }
