41a74d69ea077e5227b909d6290585c41ec61c1e
jrior001 <jriordan001@gmail.com>
2017-11-04 23:39:40 -0400

    kitakami-common: Binderize memtrack
    Change-Id: I236caf6bec7287b6824f572d5a36f03a1f874ed6

--- device-common.mk
+++ device-common.mk
@@ -152,6 +152,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8994 \
     gralloc.msm8994 \
     hwcomposer.msm8994 \
