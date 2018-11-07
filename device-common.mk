2e81650532d0fc7969a6ca210a087a9cc286c5a6
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    kitakami-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device-common.mk
+++ device-common.mk
@@ -27,6 +27,8 @@ ifneq ($(BOARD_HAVE_RADIO),false)
     $(call inherit-product, $(LOCAL_PATH)/radio.mk)
 endif
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
