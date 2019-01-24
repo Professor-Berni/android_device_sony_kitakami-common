0f9817e99d8f40331282501e93614626c2af7eef
Chippa-a <vusal1372@gmail.com>
2019-01-25 00:15:32 +0300

    kitakami-common: camera: Update Camera Wrapper
    * Thanks to Chippa-a.
    
    Change-Id: I83ed4ab1365e1642959300585933eddbe72bcd1c

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2016 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+#
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
+
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp \
-    Camera2Wrapper.cpp \
     Camera3Wrapper.cpp
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
@@ -28,22 +30,13 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libcamera_client \
     libgui \
-    libsensor \
     libutils \
-    libcutils \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    android.hidl.token@1.0-utils
 
-LOCAL_C_INCLUDES := \
-    framework/native/include \
-    frameworks/native/include/media/openmax \
-    system/media/camera/include
-
-LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_VENDOR_MODULE := true
-
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
+LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
 include $(BUILD_SHARED_LIBRARY)
