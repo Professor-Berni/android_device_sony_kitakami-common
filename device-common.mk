94bfb4b10c734b354c87bbd805a20425720ae0ea
Akash Mondal <a98mondal@gmail.com>
2019-09-28 02:22:45 +0530

    kitakami-common: build power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -278,7 +278,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
 PRODUCT_PACKAGES += \
