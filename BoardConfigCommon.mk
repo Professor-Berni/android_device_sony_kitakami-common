b910f668893c2c929f90c8bfc2004a7b8d5043a0
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    kitakami-common: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,9 +119,6 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
-# Power
-TARGET_HAS_NO_WLAN_STATS := true
-
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 
--- device-common.mk
+++ device-common.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Recovery
 PRODUCT_PACKAGES += \
