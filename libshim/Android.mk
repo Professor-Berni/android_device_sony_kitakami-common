ef4dc6963aaf34b875d8298bf8c75c9768a45fe8
TARKZiM <tom8476oo@gmail.com>
2020-02-10 13:56:13 +0800

    kitakami-common: libshim: Update GraphicBufferMapper symbols for Q

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -16,7 +16,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := camera.qcom_shim.cpp
+LOCAL_SRC_FILES := \
+    camera.qcom_shim.cpp \
+    graphic-buffer.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libgui \
