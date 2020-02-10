4bb40ba6b0fdbb839dfb5852d9c9868d3552ff66
TARKZiM <tom8476oo@gmail.com>
2020-02-10 13:57:53 +0800

    kitakami-common: fingerprint: We are using 2.1 Vendor HAL

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -33,7 +33,7 @@ namespace V2_1 {
 namespace implementation {
 
 // Supported fingerprint HAL version
-static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 0);
+static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
 
 using RequestStatus =
         android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
