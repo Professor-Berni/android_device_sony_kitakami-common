cb195d9b601a698cbff49e788dbf9a7b95a0eb46
TARKZiM <tom8476oo@gmail.com>
2020-05-08 11:55:21 +0800

    Revert "kitakami-common: Build Broadcastradio HAL"
    This reverts commit ee56166446c5d8f15050f9a9ac84ff8a5967a45d.

--- device-common.mk
+++ device-common.mk
@@ -86,7 +86,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     audio.primary.msm8994 \
     audio.r_submix.default \
     audio.usb.default \
