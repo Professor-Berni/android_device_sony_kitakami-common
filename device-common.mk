dc0bd2f4b031ca14068568e7f6afcc93e07b68e9
Bernhard Thoben <berni-the-bear@web.de>
2020-06-04 12:16:28 +0200

    Some little cleanups
    Change-Id: Ifb199bc768a70f00d8dc847188d6e9a081b84e64

--- device-common.mk
+++ device-common.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -301,10 +301,5 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
-
-# Yalpstore
-PRODUCT_PACKAGES += \
-	YalpStore \
-	privapp-permissions-YalpStore.xml
 
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
