00820ca7927ec99722db6f0a5a7cbd0b72e86146
TARKZiM <tom8476oo@gmail.com>
2020-10-08 17:06:52 +0800

    Revert "kitakami-common: Switch to legacy Wi-Fi service"
    * Seems like Google fixed the mac address issues on 11.
    
    This reverts commit 6702cc6096f95b2f63bce23b1ae9b586800cc3d9.

--- device-common.mk
+++ device-common.mk
@@ -331,7 +331,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     p2p_supplicant.conf \
     hostapd \
     libwifi-hal-bcm \
