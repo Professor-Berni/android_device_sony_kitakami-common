123f54d67869aee2fccca3ca80d3d9197a415c50
Bernhard Thoben <berni-the-bear@web.de>
2020-10-23 17:06:59 +0200

    kirakami-common: The big clean up
    Everything superfluous, obsolete, useless and duplicate removed.
    
    Change-Id: I6aa8fe596657e2b2d95de1f8b3e53edb23966126

--- device-common.mk
+++ device-common.mk
@@ -23,8 +23,7 @@ $(call inherit-product, vendor/sony/kitakami-common-extras/kitakami-common-vendo
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Radio config
 ifneq ($(BOARD_HAVE_RADIO),false)
