9119a63374ac88c5a1d5cfcd8d5c8b43fc8c1254
Bernhard Thoben <berni-the-bear@web.de>
2020-11-12 15:55:08 +0100

    Back-port a couple changes and fixes to 17.1 from joel16
    Change-Id: Iaed56ee798b80d5d220867e41c0351c284070795

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.0-service.kitakami"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.0-service.kitakami"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.kitakami"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.kitakami"
 
 #include <hardware/hw_auth_token.h>
 
@@ -184,30 +184,8 @@ Return<RequestStatus> BiometricsFingerprint::cancel() {
     return ErrorFilter(ret);
 }
 
-#define MAX_FINGERPRINTS 100
-
-typedef int (*enumerate_2_0)(struct fingerprint_device *dev, fingerprint_finger_id_t *results,
-        uint32_t *max_size);
-
 Return<RequestStatus> BiometricsFingerprint::enumerate()  {
-    fingerprint_finger_id_t results[MAX_FINGERPRINTS];
-    uint32_t n = MAX_FINGERPRINTS;
-    enumerate_2_0 enumerate = (enumerate_2_0) mDevice->enumerate;
-    int ret = enumerate(mDevice, results, &n);
-
-    if (ret == 0 && mClientCallback != nullptr) {
-        ALOGD("Got %d enumerated templates", n);
-        for (uint32_t i = 0; i < n; i++) {
-            const uint64_t devId = reinterpret_cast<uint64_t>(mDevice);
-            const auto& fp = results[i];
-            ALOGD("onEnumerate(fid=%d, gid=%d)", fp.fid, fp.gid);
-            if (!mClientCallback->onEnumerate(devId, fp.fid, fp.gid, n - i - 1).isOk()) {
-                ALOGE("failed to invoke fingerprint onEnumerate callback");
-            }
-        }
-    }
-
-    return ErrorFilter(ret);
+    return ErrorFilter(mDevice->enumerate(mDevice));
 }
 
 Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {
@@ -365,7 +343,16 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
             }
             break;
         case FINGERPRINT_TEMPLATE_ENUMERATING:
-            // ignored, won't happen for 2.0 HALs
+	            ALOGD("onEnumerate(fid=%d, gid=%d, rem=%d)",
+                msg->data.enumerated.finger.fid,
+                msg->data.enumerated.finger.gid,
+                msg->data.enumerated.remaining_templates);
+            if (!thisPtr->mClientCallback->onEnumerate(devId,
+                    msg->data.enumerated.finger.fid,
+                    msg->data.enumerated.finger.gid,
+                    msg->data.enumerated.remaining_templates).isOk()) {
+                ALOGE("failed to invoke fingerprint onEnumerate callback");
+            }
             break;
     }
 }
