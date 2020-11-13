e14474c4f65653bef28ecc62e23d70ec59b1db49
Bernhard Thoben <berni-the-bear@web.de>
2020-11-13 12:19:06 +0100

    Commonized fingerprint HAL
    Change-Id: I37a3d59fcf510871b9b8ac9d27f3b72e0b6db8f9

--- device-common.mk
+++ device-common.mk
@@ -165,6 +165,13 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8994
 
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.kitakami
+    
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
