c9c702efe579350064bc4aaf45930ffd283afbf6
Bernhard Thoben <berni-the-bear@web.de>
2020-12-11 18:29:35 +0100

    kitakami-common: Bring back FM-Radio!
    Change-Id: Ib5e6848034516afa6dad343afbb2bff5bab4281f

--- device-common.mk
+++ device-common.mk
@@ -172,6 +172,12 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8994
 
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    brcm-uim-sysfs \
+   libfmradio.v4l2-fm
+
 # Fingerprint
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
