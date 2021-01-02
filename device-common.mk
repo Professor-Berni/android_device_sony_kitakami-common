05c187cc2fcfff746b72b7bdcae9bbeacd3f398b
Bernhard Thoben <berni-the-bear@web.de>
2021-01-02 15:51:21 +0100

    kitakami-common: Removed deprecated wifi driver.
    Change-Id: Idbc65220952e791f882b00502bd8af7178c22098

--- device-common.mk
+++ device-common.mk
@@ -332,7 +332,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    libwifi-hal-bcm \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
