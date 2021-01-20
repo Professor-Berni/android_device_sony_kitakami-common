a4843cbf3d177d3dbf05d25885f441d6be92463a
Bernhard Thoben <berni-the-bear@web.de>
2021-01-20 15:39:58 +0100

    kitakami-common: Changed wifi HAL to avoid using of fake wifi addresses.
    Change-Id: I49e0db82994d809b72c03b06f5a2996cdd2c411f

--- device-common.mk
+++ device-common.mk
@@ -331,7 +331,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwpa_client \
     wpa_supplicant \
