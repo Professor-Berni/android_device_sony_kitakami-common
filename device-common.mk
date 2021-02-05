6ef1ccea45e471cc945abde72f378dca9c16a6d5
Bernhard Thoben <berni-the-bear@web.de>
2021-02-05 14:50:32 +0100

    Revert "Revert "Revert "kitakami-common: Kang Perf/Thermal stack from angler & hima"""
    This reverts commit 8e15782db832f274464745125856092be6515ae0.
    
    This restores original stock ROM thermal configuration.
    
    Change-Id: I524bbf0a176c32d4d42d9a3b84b29a35e9051d8c

--- device-common.mk
+++ device-common.mk
@@ -345,13 +345,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# Thermal
-PRODUCT_PACKAGES += \
-    thermal.qcom
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # TimeKeep
 PRODUCT_PACKAGES += \
     timekeep \
