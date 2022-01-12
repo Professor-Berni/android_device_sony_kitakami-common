c22463e93b650fc16043177d87d0cc7e280c2f60
Bernhard Thoben <berni-the-bear@web.de>
2022-01-12 18:42:54 +0100

    kitakami-common: radio: Reduced to the really needed HAL\'s.
    Change-Id: Icf103aebbd867fdfd937f6d06b206ac199592a42

--- radio.mk
+++ radio.mk
@@ -36,10 +36,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2-radio-service \
-    android.hardware.radio@1.5 \
     android.hardware.radio.config@1.0-service \
-    android.hardware.radio.config@1.2 \
-    android.hardware.radio.deprecated@1.0-service \
     android.hardware.secure_element@1.2
 
 # Telephony
