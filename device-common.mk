36fc1613422b6be235a2f1c5cefcb40299047583
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    kitakami-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device-common.mk
+++ device-common.mk
@@ -87,7 +87,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8994 \
