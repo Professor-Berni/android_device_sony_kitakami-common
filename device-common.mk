db9de452b480979bb13a589947396373b4da72c1
Bernhard Thoben <berni-the-bear@web.de>
2022-03-09 17:49:25 +0100

    kitakami-common: Removed android.hardware.keymaster@3.0-service due to too much weird error messages.
    Change-Id: Ife8ccbf4209a2e77c60e63e325c2a0fc75711fa7

--- device-common.mk
+++ device-common.mk
@@ -245,8 +245,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-impl
 
 # Lights
 PRODUCT_PACKAGES += \
