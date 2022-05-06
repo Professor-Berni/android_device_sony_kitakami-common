4afa6981248c8011d8726c91c2bc07868c73fa3f
Bernhard Thoben <berni-the-bear@web.de>
2022-05-06 15:11:51 +0200

    kitakami-common: Changed wifi HAL to avoid using of fake wifi addresses.
    * After some days of testing this way of usage of wifi HAL runs most stable.
    
    This reverts commit 0aebe2a0834debafd9f1fc9d491501bbe57fa108.

--- device-common.mk
+++ device-common.mk
@@ -399,7 +399,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwpa_client \
     WifiOverlay \
