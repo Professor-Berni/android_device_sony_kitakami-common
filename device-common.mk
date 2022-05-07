c1f3eaf07b349cffcc44d59e4da0922608c16242
Bernhard Thoben <berni-the-bear@web.de>
2022-05-07 15:35:08 +0200

    Revert "kitakami-common: Build power HAL."
    This reverts commit 45b030b68036d87479fc003df3aa84e44b48a8ac.

--- device-common.mk
+++ device-common.mk
@@ -312,8 +312,6 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
