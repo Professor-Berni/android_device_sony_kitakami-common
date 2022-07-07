5285ce9cf8ae8c5b382101fd000b63e4541183b8
TARKZiM <tom8476oo@gmail.com>
2022-07-08 02:38:47 +0800

    Revert "kitakami-common: Disable PPDaemon & Livedisplay legacymm"
    This reverts commit 0db1c41234537c8f7ea8dbc64a16cf6919abe1da.

--- device-common.mk
+++ device-common.mk
@@ -258,6 +258,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-legacymm \
     vendor.lineage.livedisplay@2.0-service-sysfs
 
 PRODUCT_PACKAGES += \
