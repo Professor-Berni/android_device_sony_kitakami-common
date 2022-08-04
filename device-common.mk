44d5d7bf416676f6cc75a08b70e8d789d5a728f3
Bernhard Thoben <berni-the-bear@web.de>
2022-08-04 11:41:22 +0200

    Revert "kitakami-common: drop Snap"
    This reverts commit 4b2f45b7b06dbc6b53eeda77e10faef8474d95f0.
    
    Change-Id: Id79a18f54f503b52f67dce6363cad0c71cd20172

--- device-common.mk
+++ device-common.mk
@@ -140,6 +140,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-kitakami \
     camera.msm8994 \
     cameraserver_kitakami \
+    Snap \
     vendor.qti.hardware.camera.device@1.0
 
 # Charger
