509992e39721269ac1d400653256ef6c99edae78
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    kitakami-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device-common.mk
+++ device-common.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # DumpState
 PRODUCT_PACKAGES += \
