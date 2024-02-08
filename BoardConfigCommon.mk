4fbd80248eb185e57be5a56c9c1111a52cef6633
Bernhard Thoben <berni-the-bear@web.de>
2024-02-08 16:03:24 +0100

    kitakami-common: Currently lib-imsvt.so is not located in /system/vendor.
    Change-Id: Ia1b63a6681117097a090b076d7d8142f8a790ae9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -221,7 +221,7 @@ TARGET_LD_SHIM_LIBS := \
 
 ifneq ($(BOARD_HAVE_RADIO),false)
 TARGET_LD_SHIM_LIBS += \
-     /system/vendor/lib64/lib-imsvt.so|libshims_ims.so \
+     /system/lib64/lib-imsvt.so|libshims_ims.so \
      /system/vendor/lib64/lib-imsdpl.so|libshims_boringssl.so \
      /system/lib64/lib-imsvideocodec.so|libshim_ui.so \
      /system/product/lib64/libimsmedia_jni.so|libshim_libimsmedia.so \
