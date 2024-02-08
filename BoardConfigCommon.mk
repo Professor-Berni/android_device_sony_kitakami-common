ccf95275bbb384a21172da2d523bd4c399bd6d79
Bernhard Thoben <berni-the-bear@web.de>
2024-02-08 16:44:56 +0100

    kitakami-common: Double declaration removed.
    Change-Id: I5d3762cb4b751aeb7cd00c400e68534fdd960e52

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -238,7 +238,6 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/wlan/parameters/firmware_path"
 WIFI_DRIVER_BUILT           := brcmfmac
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/wlan/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/wlan/bcmdhd/fw_bcmdhd_apsta.bin"
