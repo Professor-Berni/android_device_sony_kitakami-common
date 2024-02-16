beca3b70b46d2d554536a808b0e6ddf87277864a
Bernhard Thoben <berni-the-bear@web.de>
2024-02-16 21:45:02 +0100

    Revert "kitakami-common: Switched to "android.hardware.bluetooth.audio-impl"."
    This reverts commit f5f4e7dc29894c45161577370e87fdbdf3f980a5.

--- device-common.mk
+++ device-common.mk
@@ -77,7 +77,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.0 \
     android.hardware.soundtrigger@2.0-core \
     android.hardware.soundtrigger@2.0-impl \
