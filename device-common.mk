48bcae8abc6d6e314b5821b4b5e7fab839a58000
Bernhard Thoben <berni-the-bear@web.de>
2024-05-06 20:27:45 +0200

    kitakami-common: Switched to AIDL bluetooth implementation.
    Change-Id: I19667f05d50f5512d7b11ba4073fc288e8ac7116

--- device-common.mk
+++ device-common.mk
@@ -122,8 +122,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth/bt_vendor.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth-service.default \
     libbt-vendor
 
 # Charger
