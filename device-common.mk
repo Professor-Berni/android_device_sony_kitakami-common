8cabf9fdcb9e89221a8e9e423e9ca02d1a6fb91c
Bernhard Thoben <berni-the-bear@web.de>
2025-07-16 16:19:27 +0200

    kitakami-common: The entry ‘android.hardware.audio.core’ is superfluous and will therefore be removed again.
    Change-Id: Ib1722fed4f9a6c34964ca314609c9573fefe7a00

--- device-common.mk
+++ device-common.mk
@@ -70,7 +70,6 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
-    android.hardware.audio.core \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
